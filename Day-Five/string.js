1082538a5b6d3dba54b03e357662eae65cc14318
sumu-glitch <gauravparwate@gmail.com>
2025-05-30 13:14:27 +0530

    datatypes

---
+++ Day-Five/string.js
@@ -0,0 +1,33 @@
+// let name = "Gaurav"
+// let age = 25;
+// let city = "Nagpur";
+// //purane jamane ki string concatination 
+// console.log("My name is " + name + ", I am " + age + " years of old and I live in " + city + ".");
+// // Template literals (ES6)
+// console.log(`My name is ${name}, I am ${age} years of old and I live in ${city}.`);
+// //string declaration
+// const myName = new String("Gaurav");
+// //string methods
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.toLowerCase());
+// console.log(myName.charAt('2'));
+// console.log(myName.indexOf('r'));
+
+
+const myLastname = "    Kumar   ";
+console.log(myLastname);
+console.log(myLastname.trim()); // removes whitespace from both ends
+console.log(myLastname.trimStart()); // removes whitespace from the start
+console.log(myLastname.trimEnd()); // removes whitespace from the end
+const url = "https://www.example.com%20with%20spaces";
+console.log(url.replace(/%20/g, '-')); // replaces %20 with -
+console.log(url.includes("example")); // checks if the string includes "example"
+console.log(url.includes("js")); // checks if the string includes "example"
+const myString1 = "Hello, World!";
+console.log(myString1.split(", ")); // splits the string into an array at ", "
+
+
+
+
+
--- Day-Four/tempCodeRunnerFile.js
+++ Day-Four/tempCodeRunnerFile.js
@@ -1,6 +1,16 @@
-console.log(null>0);
+// console.log(null>0);
 // console.log(null==0);
 // console.log(null>=0);
 // console.log(undefined==0);
 // console.log(undefined==null);
 // console.log("2"===2);
+
+let a = 5;
+let b = a; // b is now a copy of a
+a = 10; // Changing a does not affect b
+console.log(b); // Output: 5
+
+let obj1 = { name: "Pravin" };
+let obj2 = obj1; // obj2 is a reference to obj1
+obj1.name = "Kumar"; // Changing obj1 affects obj2
+console.log(obj2.name); // Output: Kumar
